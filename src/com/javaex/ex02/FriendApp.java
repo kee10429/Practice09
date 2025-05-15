9b2a6541047deeec468e37d3e1b7fe0223c21827
kee10429 <hangijeong9@gmail.com>
2025-05-15 17:44:49 +0900

    2025-05-15

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,11 +1,20 @@
 package com.javaex.ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
 
-
+    	ArrayList fList = new ArrayList();
+    	
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("친구를 3명 등록해 주세요");
+    	ArrayList = sc.nextLine();
+    	fList hp = sc.nextLine();
+    	
     	
     	
     }
